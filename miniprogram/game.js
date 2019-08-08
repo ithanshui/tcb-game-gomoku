271bfa5ce17e6bc6dd79e5dc67642d56ed21fd9e
yuanxindong <yuanxin.me@gmail.com>
2019-08-08 18:16:20 +0800

    update: 游戏入口文件逻辑

--- miniprogram/game.js
+++ miniprogram/game.js
@@ -1,14 +1,12 @@
-import * as cloud from './shared/cloud.js'
+import './sdk/cloudsdk.js'
 import drawBackground from './modules/background.js'
 import drawHead from './modules/head.js'
-import drawChessmen from './modules/chessmen.js'
+import chessmen from './modules/chessmen.js'
 
 async function main () {
-  const { db } = cloud
-
   await drawBackground()
   await drawHead()
-  await drawChessmen()
+  chessmen.draw()
 }
 
 main()

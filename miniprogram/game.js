e0a90a76585b0605f9e062f5d73615f724bfaf88
yuanxindong <yuanxin.me@gmail.com>
2019-08-09 14:28:45 +0800

    change

--- miniprogram/game.js
+++ miniprogram/game.js
@@ -2,6 +2,7 @@ import './sdk/cloudsdk.js'
 import drawBackground from './modules/background.js'
 import drawHead from './modules/head.js'
 import chessmen from './modules/chessmen.js'
+import player from './modules/palyer.js'
 
 async function main () {
   await drawBackground()
